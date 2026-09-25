f27489f5d1b5787e09088247c97a1e7778383f60
Mr. Denman Mills <alexmillsmail@gmail.com>
2026-09-25 13:24:59 -0400

    proof: sync src/bmscl_runtime_sup.erl

--- beamscale-proof/src/bmscl_runtime_sup.erl
+++ beamscale-proof/src/bmscl_runtime_sup.erl
@@ -16,10 +16,14 @@ start_link() ->
     end.
 
 service_names() ->
-    Base = [bmscl_invocation_sup, bmscl_guest_control, bmscl_route_refresh,
-            bmscl_route_redis_source, bmscl_route_table, bmscl_phoenix_connections,
-            bmscl_durable_registry, bmscl_budget_manager, bmscl_authority_broker,
-            bmscl_deployment_manager],
+    Base0 = [bmscl_invocation_sup, bmscl_guest_control, bmscl_route_refresh,
+             bmscl_route_redis_source, bmscl_route_table, bmscl_phoenix_connections,
+             bmscl_durable_store_redis, bmscl_durable_registry, bmscl_critical_section_registry, bmscl_env_cache, bmscl_middleware_chain,
+             bmscl_budget_manager, bmscl_authority_broker, bmscl_deployment_manager],
+    Base = case bmscl_provider_runtime:mode() of
+        native -> Base0;
+        _ -> [bmscl_provider_runtime | Base0]
+    end,
     case application:get_env(bmscl_supervisor, http_provider_module, undefined) of
         bmscl_httpc_provider -> [bmscl_http_request_manager | Base];
         _ -> Base
@@ -49,6 +53,12 @@ init([]) ->
           shutdown => 5000,
           type => worker,
           modules => [bmscl_deployment_manager]},
+        #{id => bmscl_env_cache,
+          start => {bmscl_env_cache, start_link, []},
+          restart => permanent,
+          shutdown => 5000,
+          type => worker,
+          modules => [bmscl_env_cache]},
         #{id => bmscl_authority_broker,
           start => {bmscl_authority_broker, start_link, []},
           restart => permanent,
@@ -67,18 +77,36 @@ init([]) ->
           shutdown => 5000,
           type => worker,
           modules => [bmscl_route_table]},
+        #{id => bmscl_middleware_chain,
+          start => {bmscl_middleware_chain, start_link, []},
+          restart => permanent,
+          shutdown => 5000,
+          type => worker,
+          modules => [bmscl_middleware_chain]},
         #{id => bmscl_phoenix_connections,
           start => {bmscl_phoenix_connections, start_link, []},
           restart => permanent,
           shutdown => 5000,
           type => worker,
           modules => [bmscl_phoenix_connections]},
+        #{id => bmscl_durable_store_redis,
+          start => {bmscl_durable_store_redis, start_link, []},
+          restart => permanent,
+          shutdown => 5000,
+          type => worker,
+          modules => [bmscl_durable_store_redis]},
         #{id => bmscl_durable_registry,
           start => {bmscl_durable_registry, start_link, []},
           restart => permanent,
           shutdown => 5000,
           type => worker,
-          modules => [bmscl_durable_registry]}
+          modules => [bmscl_durable_registry]},
+        #{id => bmscl_critical_section_registry,
+          start => {bmscl_critical_section_registry, start_link, []},
+          restart => permanent,
+          shutdown => 5000,
+          type => worker,
+          modules => [bmscl_critical_section_registry]}
     ],
     Suffix = [
         #{id => bmscl_route_refresh,
@@ -100,7 +128,8 @@ init([]) ->
           type => worker,
           modules => [bmscl_guest_control]}
     ],
-    Children = Prefix ++ http_request_children() ++ route_source_children() ++ Suffix,
+    Children = Prefix ++ http_request_children() ++ route_source_children() ++
+               Suffix ++ provider_children(),
     {ok, {{one_for_all, 5, 10}, Children}}.
 
 http_request_children() ->
@@ -127,4 +156,17 @@ route_source_children() ->
                modules => [bmscl_route_redis_source]}];
         _ ->
             []
+    end.
+
+
+provider_children() ->
+    case bmscl_provider_runtime:mode() of
+        native -> [];
+        _ ->
+            [#{id => bmscl_provider_runtime,
+               start => {bmscl_provider_runtime, start_link, []},
+               restart => permanent,
+               shutdown => 10000,
+               type => worker,
+               modules => [bmscl_provider_runtime]}]
     end.
